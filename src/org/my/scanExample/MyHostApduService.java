df6a9f38469af038b839fee03c88c1f04bc3899d
Mehul Patel <patelmehul0619@gmail.com>
2014-05-11 23:14:36 -0400

    new Thread added

--- src/org/my/scanExample/MyHostApduService.java
+++ src/org/my/scanExample/MyHostApduService.java
@@ -10,26 +10,43 @@ public class MyHostApduService extends HostApduService {
 	final static String TAG = MyHostApduService.class.getSimpleName();
 	String cardInfo = null;
 	
-	
 	//This function start the service, collect credit card information passed from MyScanActivity 
 	//and pass to the Android Device. 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
+		
+		Log.i(TAG, "HostAdpuService Started!!!");
+		
+		
 		Bundle b=intent.getExtras();
 		cardInfo = b.getString("resultStr");
-		Log.i(TAG, "HostAdpuService Started!!!");
-		byte[] apdu = cardInfo.getBytes();
 		
-		//This function send data from MyHostApduService to Android Device 
-		sendResponseApdu(apdu);
-		return super.onStartCommand(intent, flags, startId);
-	}
+		final byte[] apdu = cardInfo.getBytes();
+		
+		//This thread is created to perform data transfer so that main thread can relieved to perform its tasks.
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
 
-	
+				Log.i(TAG, "Inside new Thread!");
+				
+				//This function send data from MyHostApduService to Android Device 
+				sendResponseApdu(apdu);
+			}
+		}).start();
+		
+		//return super.onStartCommand(intent, flags, startId);
+		
+		//re-deliver intent in case service's process get killed after it started
+		return START_REDELIVER_INTENT;
+	}
 
 	//This function receive response from Android Device
 	@Override
 	public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+		
+		Log.i(TAG, "Inside processCommandApdu!");
 			
 		String response = commandApdu.toString();
 		Log.i(TAG, response);
@@ -37,16 +54,13 @@ public class MyHostApduService extends HostApduService {
 		return commandApdu;
 	
 	}
-
-	
 	
 	@Override
 	public void onDeactivated(int reason) {
 		
+		Log.i(TAG, "Inside onDeactivated!");
+		
 		Log.i(TAG, "Deactivated: " + reason);
 		
 	}
-	
-	
-
 }
